b03d47de64a276a084e0a3431790e0d5cdc797fc
nitin-175 <nitinkumarmaurya457@gmail.com>
2025-10-22 19:14:11 +0530

    backend services connected

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,38 @@
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-import "./App.css";
 import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
-
-
-
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./ui/AppLayout";
 
 function App() {
-	return (
-		<>
-			<GlobalStyles />
-      <Row>
+  return (
+    <>
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
 
-			<div>
-				<Heading type='h1'>The Wild Oasis</Heading>
-				<Button onClick={() => alert("check in")}>Check in</Button>
-				<Input type="number" placeholder="Number of guests" />
-			</div>
-      
-      </Row>
-		</>
-	);
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/services/supabase.js
@@ -0,0 +1,8 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'https://ibtryzoaophocyxwberi.supabase.co'
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlidHJ5em9hb3Bob2N5eHdiZXJpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5NDUyNDQsImV4cCI6MjA3NjUyMTI0NH0.618oqVSZpvOKZFJza5ElcmNPfYNLV5Je07GcmKGQAFw";
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+
+export default supabase;
